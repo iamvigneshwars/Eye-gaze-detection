4272608dcc5af02550590d9b7e98124678355096
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-27 16:46:00 +0530

    eye blinking both eyes

--- eye-detection.py
+++ eye-detection.py
@@ -14,6 +14,18 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 def midpoint(p1, p2):
     return int((p1.x + p2.x) /2), int((p1.y + p2.y)/2)
 
+def blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+    hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+    ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+    hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+    ratio = hor_line_lenght / ver_line_lenght
+    return ratio
+
 
 while True:
     _, frame = cap.read()
@@ -22,24 +34,11 @@ while True:
 
     for face in faces:
         landmarks = predictor(gray, face)
-        #for i in range(36, 48):
-        #    x = landmarks.part(i).x
-        #    y = landmarks.part(i).y
-        #    cv2.circle(frame, (x, y), 1, (0,250,0), 1)
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
-        center_top = midpoint(landmarks.part(37), landmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
 
-        hor_line = cv2.line(frame, left_point, right_point, (0, 250, 0),1)
-        ver_line = cv2.line(frame, center_top, center_bottom, (0,250, 0),1)
-
-        hor_line_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
-        ver_line_length = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
-        #ratio = hor_line_length/ver_line_length
-        print(ver_line_length)
-
-        if ver_line_length < 12:
+        left_eye_ratio = blinking_ratio([36,37,38,39,40,41], landmarks)
+        right_eye_ratio = blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        
+        if ((left_eye_ratio + right_eye_ratio)/2) > 5.7:
         	cv2.putText(frame, "BLINKING", (50, 150), font, 3, (0, 0, 255))
 
 
