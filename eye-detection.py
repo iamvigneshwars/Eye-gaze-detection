75621016e4153e155b11b3a7523af522d0a6bb11
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-27 15:27:20 +0530

    Face Detection

--- eye-detection.py
+++ eye-detection.py
@@ -4,11 +4,21 @@ import dlib
 
 cap = cv2.VideoCapture(2)
 
+detector = dlib.get_frontal_face_detector()
+
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
 while True:
     _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
 
+    for face in faces:
+        x, y = face.left(), face.top()
+        x1, y1 = face.right(), face.bottom()
+        cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
     cv2.imshow("Frame", frame)
-
+    
     key = cv2.waitKey(1)
     if key == 27:
         break
